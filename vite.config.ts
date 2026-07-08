02d4d7701805b267cbede2c7ef89a1e6b040dad1
CloudeaSoft <cloudeasoft@qq.com>
2026-07-08 17:51:44 +0800

    chore: integrate vite as build tool

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: () => 'index.js',
+    },
+    rollupOptions: {
+      external: [],
+    },
+    sourcemap: true,
+    target: 'esnext',
+    outDir: 'lib',
+    emptyOutDir: false,
+  },
+})
